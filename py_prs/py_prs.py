0d7e81ce7a54aab57d1e02fced8589aa824a3986
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2021-10-20 14:48:28 +0100

    Use double quotes for consistency

--- py_prs/py_prs.py
+++ py_prs/py_prs.py
@@ -2,16 +2,16 @@ import argparse
 from . import reviews
 from . import config
 
-parser = argparse.ArgumentParser(description='')
+parser = argparse.ArgumentParser(description="")
 
-subparsers = parser.add_subparsers(help='')
+subparsers = parser.add_subparsers(help="")
 
-parser_set = subparsers.add_parser('set', help='Set the GitHub User ID or access token')
-parser_set.add_argument('--user', type=str, nargs=1, help='GitHub User ID')
-parser_set.add_argument('--token', type=str, nargs=1, help='GitHub Access Token')
+parser_set = subparsers.add_parser("set", help="Set the GitHub User ID or access token")
+parser_set.add_argument("--user", type=str, nargs=1, help="GitHub User ID")
+parser_set.add_argument("--token", type=str, nargs=1, help="GitHub Access Token")
 parser_set.set_defaults(func=config.set)
 
-parser_list = subparsers.add_parser('list', help='List PRs that need reviewing')
+parser_list = subparsers.add_parser("list", help="List PRs that need reviewing")
 parser_list.set_defaults(func=reviews.get_review_requests)
 
 def parse_command():

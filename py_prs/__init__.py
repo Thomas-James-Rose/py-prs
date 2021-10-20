a16d930c165518fdae036006d7a67c6e35773ed4
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2021-10-20 14:49:18 +0100

    Remove unnecessary duplication

--- py_prs/__init__.py
+++ py_prs/__init__.py
@@ -3,9 +3,7 @@ from . import reviews
 from . import config
 import os
 
-config_path = f"{os.path.expanduser('~')}/.pyprs/config.json"
-
-os.makedirs(os.path.dirname(config_path), exist_ok=True)
-open(config_path, "w")
+os.makedirs(os.path.dirname(config.config_path), exist_ok=True)
+open(config.config_path, "w")
 
 parse = py_prs.parse_command

d3b9ea71386d6c0ec49e9c1ba5f80a11c1e03e72
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2021-10-20 14:38:02 +0100

    Implement set command to allow a GitHub user ID and access token to be configured

--- py_prs/config.py
+++ py_prs/config.py
@@ -1,3 +1,27 @@
+import json
+import os
+
+config_path = f"{os.path.expanduser('~')}/.pyprs/config.json"
+
 def set(args):
-  print('Setting up user...')
-  print(args)
+  os.makedirs(os.path.dirname(config_path), exist_ok=True)
+  open(config_path, "w")
+
+  with open(config_path, "r") as config_file:
+    try:
+      config = json.load(config_file)
+    except:
+      config = {}
+
+  with open(config_path, "w+") as config_file:
+    if args.user != None:
+      config["user"] = args.user
+
+    if args.token != None:
+      config["token"] = args.token
+
+    config_file.write(json.dumps(config))
+
+  with open(config_path, "r") as config_file:
+    config = json.load(config_file)
+    print(config)
--- py_prs/py_prs.py
+++ py_prs/py_prs.py
@@ -8,6 +8,7 @@ subparsers = parser.add_subparsers(help='')
 
 parser_set = subparsers.add_parser('set', help='Set the GitHub User ID or access token')
 parser_set.add_argument('--user', type=str, nargs=1, help='GitHub User ID')
+parser_set.add_argument('--token', type=str, nargs=1, help='GitHub Access Token')
 parser_set.set_defaults(func=config.set)
 
 parser_list = subparsers.add_parser('list', help='List PRs that need reviewing')

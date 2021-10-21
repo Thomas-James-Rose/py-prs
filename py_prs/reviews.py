6cec38f45fe068beef0717c708a0ec40bf78194f
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2021-10-21 08:59:59 +0100

    Use the configured user and access token to make the GitHub request

--- py_prs/reviews.py
+++ py_prs/reviews.py
@@ -1,9 +1,23 @@
+import json
 import requests
+from string import Template
+from . import config
 
 def get_review_requests(args):
-  query = """
+  with open(config.config_path, "r") as config_file:
+    user_config = json.load(config_file)
+
+  if "user" not in user_config:
+    print("A user has not been configured. Please use 'pyprs set --user <GITHUB_USER_ID>' to configure one.")
+    return
+  
+  if "token" not in user_config:
+    print("An access token has not been configured. Please use 'pyprs set --token <GITHUB_ACCESS_TOKEN>' to configure one.")
+    return
+
+  query = Template("""
   {
-    search(query: "type:pr state:open review-requested:<USER_ID>", type: ISSUE, first: 100) {
+    search(query: "type:pr state:open review-requested:$user_id", type: ISSUE, first: 100) {
       issueCount
       pageInfo {
         endCursor
@@ -22,8 +36,8 @@ def get_review_requests(args):
       }
     }
   }
-  """
+  """).substitute(user_id=user_config["user"][0])
 
-  response = requests.post("https://api.github.com/graphql", json={"query": query}, headers={"Authorization": "bearer <GH_ACCESS_TOKEN>"})
-  print(response.status_code)
+  auth_header = Template("bearer $access_token").substitute(access_token=user_config["token"][0])
+  response = requests.post("https://api.github.com/graphql", json={"query": query}, headers={"Authorization": auth_header})
   print(response.text)
